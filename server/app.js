2c984ee54f7c43453952817d8aa4729778b145dc
ifigolds <ifigolds@gmail.com>
2026-04-17 05:29:18 +0300

    fix cors

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const adminRoutes = require('./routes/admin');
+const cors = require('cors')
 
 const { initDb } = require('./database');
 
